146496b089a8a499b8d9f3bf90607799f70bf816
Guido Sabik <guido.sabik@gmail.com>
2024-01-22 17:25:08 -0300

    first commit

---
+++ components/Hero.tsx
@@ -0,0 +1,12 @@
+const Hero = async () => {
+  return (
+    <section className="flex flex-col h-screen items-center justify-center">
+        <div className="space-y-2">
+            <h1 className="border-b pb-1 text-3xl text-center font-semibold tracking-tight first:mt-0">MP Yield</h1>
+            <p>Seguí y calculá el rendimiento que genera <span className="font-semibold">Mercado Pago</span> día a día</p>
+        </div>
+    </section>
+  );
+}
+
+export default Hero
---
+++ lib/scraper.ts
@@ -0,0 +1,21 @@
+import puppeteer from "puppeteer";
+
+export const scrapeMercadoPago = async() => {
+	const MPURL = "https://www.mercadopago.com.ar/cuenta";
+
+	const browser = await puppeteer.launch();
+	const page = await browser.newPage();
+	await page.goto(MPURL);
+
+	// Scrape yield
+	const scrapeText = await page.evaluate(() => {
+		const text = document.querySelector(".basics .cards div:first-child p")?.innerHTML;
+		return text;
+	});
+
+	// Regex text scrape
+	const regex = /(\d+(\.\d+)?)/g;
+	const result = scrapeText?.match(regex);
+
+	return result;
+}
