7277d72816c53eb41de6f9a4aa0186957336430e
Guido Sabik <guido.sabik@gmail.com>
2024-02-19 10:44:03 -0300

    add responsive styles on Hero component

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -5,7 +5,7 @@ const Hero = () => {
     <section className="flex flex-col h-screen items-center justify-center">
       <div className="flex flex-col justify-center  items-center space-y-2">
         <h1 className="border-b pb-1 text-4xl font-semibold tracking-tight first:mt-0">MP Yield</h1>
-        <p>Seguí y calculá el rendimiento que genera <a className="font-semibold text-gray-400 hover:text-gray-300 transition-all" href="https://www.mercadopago.com.ar/cuenta" target="_blank">Mercado Pago</a> día a día</p>
+        <p className="text-center">Seguí y calculá el rendimiento que genera <a className="font-semibold text-gray-400 hover:text-gray-300 transition-all" href="https://www.mercadopago.com.ar/cuenta" target="_blank">Mercado Pago</a> día a día</p>
         <Link className="text-gray-400 underline underline-offset-2 hover:text-gray-300 transition-all" href="/#yield">Ver y calcular</Link>
       </div>
     </section>
